f46a97c92fe0b697da9e70215d1cf30bc709d9d7
Anthony Cook <anthonymichaelcook@gmail.com>
2012-04-27 18:02:22 -0500

    Adding core bits to start testing this functionality to ensure stability.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source :rubygems
+
+group :test do
+  gem 'rspec'
+
+  gem 'pry'
+  gem 'open4'
+
+  gem 'growl'
+  gem 'guard'
+  gem 'guard-rspec'
+
+  gem 'activesupport', :require => 'active_support/core_ext/object/blank'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+$: << File.join(File.dirname(__FILE__), '/../lib')
+
+require 'bundler'
+Bundler.require(:test)
+
+def run(*args)
+  output = Struct.new(:pid, :stdout, :stderr)
+
+  status = Open4.popen4(*args) do |pid, stdin, stdout, stderr|
+    output = output.new pid, stdout.read, stderr.read
+  end
+
+  [status, output]
+end
