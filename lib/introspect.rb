7adead21d22e3bb841f49d0551122b868c1061b5
Anthony Cook <github@anthonymcook.com>
2016-12-02 14:23:46 -0600

    Moving contents into its own object.

--- lib/introspect.rb
+++ lib/introspect.rb
@@ -9,63 +9,18 @@
 # By default there is no depth limit. Pass in 1 to just get info on the object itself.
 #
 
-require "introspect/version"
+require 'introspect/version'
+require 'introspect/contents'
 
 module Introspect
 
   # command handler, this will let me namespace things better later
-  def introspect command = nil, arg = nil
-    message = []
-    if command.is_a?(Symbol) then
-      message << "introspect_#{command}"
-    else
-      message << :introspect_contents
-      message << command if command
+  def introspect command = nil, opts = nil
+    case command
+    when :name then self.introspect_name
+    when :contents then Contents.contents self, opts
+    else Contents.contents self
     end
-
-    message << arg if arg
-
-    self.send *message
-  end
-
-  def introspect_contents depth = 0
-
-    if self.kind_of?(Module) && ![Object, Module, Class, BasicObject, Kernel].include?(self)
-      @ancestors = self.ancestors
-      @ancestors.shift
-
-      @constants = []
-      self.constants.sort.each do |c|
-        object = self.const_get(c)
-
-        if object.respond_to?(:contents) && (depth == 0 || depth > 1) then
-          @constants << object.contents(depth == 0 ? 0 : depth - 1)
-        else
-          @constants << object.introspect(:name)
-        end
-      end
-
-      @methods = []
-      self.methods(false).each do |m|
-        @methods << self.method(m)
-      end
-
-      @instance_methods = []
-      self.instance_methods(false).each do |m|
-        @instance_methods << self.instance_method(m)
-      end
-    else
-      @ancestors = self.class.ancestors
-    end
-
-    structure = Hash.new
-    structure[:class] = self.class
-    structure[:ancestors] = @ancestors unless @ancestors.nil? || @ancestors.empty?
-    structure[:constants] = @constants unless @constants.nil? || @constants.empty?
-    structure[:methods] = @methods unless @methods.nil? || @methods.empty?
-    structure[:instance_methods] = @instance_methods unless @instance_methods.nil? || @instance_methods.empty?
-
-    {self.introspect(:name) => structure}
   end
 
   def introspect_name
@@ -74,13 +29,5 @@ module Introspect
     else
       self
     end
-  end
-
-  def introspect_which symbol
-    klass = self.is_a?(Class) ? self : self.class
-
-    klass.ancestors.select do |dad|
-      dad.instance_methods.include?(symbol) || dad.methods.include?(symbol)
-    end.last
   end
 end
---
+++ lib/introspect/contents.rb
@@ -0,0 +1,50 @@
+class Introspect::Contents
+  def self.contents object, arg = nil
+    self.new(object).contents arg
+  end
+
+  def initialize object
+    @object = object
+  end
+
+  def contents depth = 0
+    depth = 0 if depth.nil?
+
+    if @object.kind_of?(Module) && ![Object, Module, Class, BasicObject, Kernel].include?(@object)
+      @ancestors = @object.ancestors
+      @ancestors.shift
+
+      @constants = []
+      @object.constants.sort.each do |c|
+        object = @object.const_get(c)
+
+        if object.respond_to?(:contents) && (depth == 0 || depth > 1) then
+          @constants << object.contents(depth == 0 ? 0 : depth - 1)
+        else
+          @constants << object.introspect(:name)
+        end
+      end
+
+      @methods = []
+      @object.methods(false).each do |m|
+        @methods << @object.method(m)
+      end
+
+      @instance_methods = []
+      @object.instance_methods(false).each do |m|
+        @instance_methods << @object.instance_method(m)
+      end
+    else
+      @ancestors = @object.class.ancestors
+    end
+
+    structure = Hash.new
+    structure[:class] = @object.class
+    structure[:ancestors] = @ancestors unless @ancestors.nil? || @ancestors.empty?
+    structure[:constants] = @constants unless @constants.nil? || @constants.empty?
+    structure[:methods] = @methods unless @methods.nil? || @methods.empty?
+    structure[:instance_methods] = @instance_methods unless @instance_methods.nil? || @instance_methods.empty?
+
+    {@object.introspect(:name) => structure}
+  end
+end
